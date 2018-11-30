f3ad98e994635420581365f0f577e323f2b84b64
jagothjdey <39712897+jagothjdey@users.noreply.github.com>
2018-11-30 04:35:56 +0100

    Set Up Project using H2 Database

---
+++ src/main/java/com/spring/database/databasedemo/DatabaseDemoApplication.java
@@ -0,0 +1,19 @@
+package com.spring.database.databasedemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseDemoApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+}
